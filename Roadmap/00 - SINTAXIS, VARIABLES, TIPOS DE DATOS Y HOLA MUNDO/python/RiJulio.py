c2678e2f82e445b5723111120977d61333d0f7c7
RiJulio <ricardijulio21@gmail.com>
2025-03-29 18:36:15 -0300

    Create RiJulio.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/RiJulio.py
@@ -0,0 +1,36 @@
+#Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+# https://www.python.org/
+
+# Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+# Esto es un comentario en una linea.
+
+"""Esto es un 
+comentario en 
+varias lineas."""
+
+'''Los comentarios en multilinea 
+se pueden hacer con comillas
+dobles o simples'''
+
+# Crea una variable (y una constante si el lenguaje lo soporta).
+
+my_variable = "Esta variable es un string (str)"
+
+#Como python no tiene la sintaxis para establecer una constante, se establece por convencion 
+
+MY_CONSTANT = "Esta es mi constante"
+
+#Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+
+my_int = 20
+my_float = 20.5
+my_str = "Un string otra vez xD"
+my_bool = True
+my_second_bool = False
+
+#- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+my_message = "¡Hola, Python!"
+print(my_message)
